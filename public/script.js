13c492a73a4215998c5fef1441820987cec6cbf4
unknown <smolutsha@gmail.com>
2023-10-25 11:18:58 +0200

    third committs

--- public/script.js
+++ public/script.js
@@ -1,31 +1,18 @@
-  // Function to generate and download the PDF
-  function generatePDF() {
-    const element = document.getElementById('third.html'); // Replace with your HTML element's ID
-    const pdfOptions = {
-      margin: 10,
-      filename: 'generated-pdf.pdf',
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
-    };
+import html2pdf from './html2pdf.js';
 
-    html2pdf().from(element).set(pdfOptions).outputPdf().then(function (pdf) {
-      // Save the PDF as a blob
-      const blob = new Blob([pdf], { type: 'application/pdf' });
+// Function to generate and download the PDF
+function generatePDF() {
+  const element = document.getElementById('third.html'); // Replace 'your-element-id' with the actual ID of your HTML element
+  const pdfOptions = {
+    margin: 10,
+    filename: 'generated-pdf.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+  };
 
-      // Create a temporary link to trigger the download
-      const link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      link.download = pdfOptions.filename;
-      link.style.display = 'none';
+  html2pdf().from(element).set(pdfOptions).save();
+}
 
-      // Trigger the download and remove the link
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    });
-  }
-
-  // Attach the generatePDF function to the button click event
-  document.getElementById('generate-pdf').addEventListener('click', generatePDF);
-
+// Attach the generatePDF function to the button click event
+document.getElementById('generate-pdf').addEventListener('click', generatePDF);
